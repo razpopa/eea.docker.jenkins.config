f2c701ccf9127bd3f17e95178ee1173540efc7f8
Alin Voinea <alin@eaudeweb.ro>
2015-03-13 15:37:13 +0000

    Refs #23835 - Script to extract installed plugins

---
+++ plugins.py
@@ -0,0 +1,16 @@
+import xml.etree.ElementTree as ET
+
+def main():
+    tree = ET.parse('installedPlugins.xml')
+    root = tree.getroot()
+
+    plugins = []
+    for entry in root.getchildren()[0]:
+        plugin, version = entry.getchildren()
+        plugins.append('%s:%s\n' % (plugin.text, version.text))
+
+    with open('plugins.txt', 'w') as ofile:
+        ofile.writelines(plugins)
+
+if __name__ == '__main__':
+    main()
